ad74ca71fb0355657b7a96927de68a29e9df9aba
racmathafidz <rac.hafidz@gmail.com>
2021-06-19 08:39:44 +0700

    add nprogress module for loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+import "../styles/nprogress.css";
 import '../styles/globals.css'
 import '../styles/swiper.css'
 import '../styles/react-tabs.css'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
