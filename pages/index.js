10ae9832ed0c7d0397f1de47daad6da75f0e9a24
racmathafidz <rac.hafidz@gmail.com>
2021-06-22 23:45:31 +0700

    filtering data who have backdrop and poster image

--- pages/index.js
+++ pages/index.js
@@ -34,34 +34,55 @@ export default function Home(props) {
 export const getStaticProps = async () => {
   const allTrendingRes = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.API_KEY}`)
   const allTrending = await allTrendingRes.json()
+  const filteredAllTrendingData = allTrending.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const movieTrendingRes = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`)
   const movieTrending = await movieTrendingRes.json()
+  const filteredMovieTrendingData = movieTrending.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const tvTrendingRes = await fetch(`https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.API_KEY}`)
   const tvTrending = await tvTrendingRes.json()
+  const filteredTvTrendingData = tvTrending.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const moviePopularRes = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
   const moviePopular = await moviePopularRes.json()
+  const filteredMoviePopularData = moviePopular.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const tvPopularRes = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
   const tvPopular = await tvPopularRes.json()
+  const filteredTvPopularData = tvPopular.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const movieInTheaterRes = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`)
   const movieInTheater = await movieInTheaterRes.json()
+  const filteredMovieInTheaterData = movieInTheater.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   const tvAiringRes = await fetch(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}&language=en-US&page=1`)
   const tvAiring = await tvAiringRes.json()
+  const filteredTvAiringData = tvAiring.results.filter( items => {
+    return items.backdrop_path !== null && items.poster_path !== null
+  })
 
   return {
     props: {
-      allTrending: allTrending.results,
-      movieTrending: movieTrending.results,
-      tvTrending: tvTrending.results,
-      moviePopular: moviePopular.results,
-      tvPopular: tvPopular.results,
-      movieInTheater: movieInTheater.results,
-      tvAiring: tvAiring.results,
+      allTrending: filteredAllTrendingData,
+      movieTrending: filteredMovieTrendingData,
+      tvTrending: filteredTvTrendingData,
+      moviePopular: filteredMoviePopularData,
+      tvPopular: filteredTvPopularData,
+      movieInTheater: filteredMovieInTheaterData,
+      tvAiring: filteredTvAiringData
     }
   }
 };
