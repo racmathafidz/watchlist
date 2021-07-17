885a1ef85db60f68ed5839fe2028f60c25039abc
racmathafidz <rac.hafidz@gmail.com>
2021-07-17 10:38:38 +0700

    fix header

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { Transition } from '@headlessui/react'
 import axios from 'axios'
@@ -26,6 +27,13 @@ export default function Header() {
 
     const isActiveLink = href => {
         return router.asPath === href ? "active-link" : ""
+    }
+
+    const imageLoader = ({ src }) => {
+      if ( src === "/" ) {
+          return 'https://image.freepik.com/free-vector/3d-realistic-illustration-open-movie-clapperboard-clapper-isolated-background_1441-1783.jpg'
+      }
+      return `https://image.tmdb.org/t/p/w500/${src}`
     }
 
     const handleSearchChange = async (event) => {
@@ -90,7 +98,7 @@ export default function Header() {
 
                 <Link href="/search">
                     <a>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white mr-4 transform transition duration-300 hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white mr-1 transform transition duration-300 hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </a>
@@ -105,15 +113,21 @@ export default function Header() {
                 enterTo="opacity-100"
                 leave="transition-opacity duration-150"
                 leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                className="absolute z-40 bg-black w-screen h-screen flex flex-col items-center mt-14 pt-14 sm:mt-180 pb-28 border-b border-gray-800"
+                leaveTo="opacity-0"    
+                className="absolute z-40 w-screen left-0"            
             >
-                <Link href="/movie">
-                    <a className={`${isActiveLink("/movie")} text-2xl font-title mb-3 transform transition duration-300 hover:text-gray-300`}>Movies</a>
-                </Link>
-                <Link href="/tv">
-                    <a className={`${isActiveLink("/tv")} text-2xl font-title mb-10 transform transition duration-300 hover:text-gray-300`}>TV Shows</a>
-                </Link>
+                <ul className="absolute z-40 bg-black w-full left-0 h-screen flex flex-col items-center pt-40 sm:pt-48 border-b border-gray-800">
+                    <li className="mb-2 sm:mb-3">
+                        <Link href="/movie">
+                            <a className={`${isActiveLink("/movie")} text-2xl sm:text-3xl lg:text-4xl font-title transform transition duration-300 hover:text-gray-300`}>Movies</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/tv">
+                            <a className={`${isActiveLink("/tv")} text-2xl sm:text-3xl lg:text-4xl font-title transform transition duration-300 hover:text-gray-300`}>TV Shows</a>
+                        </Link>
+                    </li>
+                </ul>
             </Transition>
 
             {/** For Desktop Ver. */}
@@ -151,21 +165,35 @@ export default function Header() {
                             <input 
                                 type="text" 
                                 placeholder="Search" 
-                                className="w-60 text-lg font-title bg-black focus:outline-none" 
+                                className="w-68 mr-4 text-lg font-title bg-black focus:outline-none" 
                                 onChange={handleSearchChange}                                                            
                                 value={value}
                             />                            
                             {
                                 searchActive && searchResults !== undefined && (
-                                    <ul className="w-60 bg-black font-title absolute z-50 mt-9 border border-truegray-800 rounded">
+                                    <ul className="w-68 mr-4 bg-black font-title absolute z-50 mt-9 border border-truegray-800 rounded">
                                         {searchResults.map( items => (
                                             <li className="p-2 border-b border-truegray-700 hover:text-truegray-300">
                                                 <Link                                                 
                                                     href={`/${items.media_type}/${items.id}`}
                                                     key={ items.id }
                                                 >
-                                                    <a>
-                                                        {items.media_type === "tv" ? items.name : items.title}
+                                                    <a className="flex items-center">
+                                                        <Image
+                                                          loader={imageLoader}
+                                                          src={items.poster_path === null ? "/" : items.poster_path}
+                                                          alt="poster"
+                                                          width={30}
+                                                          height={45}
+                                                        />
+                                                        <div className="ml-2 flex flex-col">
+                                                            <h1 className="text-base">
+                                                                {items.media_type === "tv" ? items.name : items.title}
+                                                            </h1>
+                                                            <p className="text-sm italic text-truegray-200">
+                                                                {items.media_type === "tv" ? "TV Series" : "Movie"}
+                                                            </p>
+                                                        </div>
                                                     </a>
                                                 </Link>
                                             </li>
